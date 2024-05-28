00ca61ff0b3c420a41c821dc7078239e5ca65b03
Danilich <danilakop03@gmail.com>
2024-05-28 14:54:37 +0700

    initial commit

---
+++ lab 2 (exchange rates)/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿using System.Windows.Forms.DataVisualization.Charting;
+
+namespace lab_2__exchange_rates_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.forecastRange = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(23)))), ((int)(((byte)(24)))));
+            chartArea2.AxisX.LabelStyle.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            chartArea2.AxisX.LabelStyle.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            chartArea2.AxisX.LabelStyle.Format = "0";
+            chartArea2.AxisX.LineColor = System.Drawing.SystemColors.AppWorkspace;
+            chartArea2.AxisX.LineWidth = 3;
+            chartArea2.AxisX.MajorGrid.LineColor = System.Drawing.SystemColors.ControlDarkDark;
+            chartArea2.AxisX.Title = "Дни";
+            chartArea2.AxisX.TitleFont = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            chartArea2.AxisX.TitleForeColor = System.Drawing.SystemColors.AppWorkspace;
+            chartArea2.AxisY.LabelStyle.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            chartArea2.AxisY.LabelStyle.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            chartArea2.AxisY.LabelStyle.Format = "0.00";
+            chartArea2.AxisY.LineColor = System.Drawing.SystemColors.AppWorkspace;
+            chartArea2.AxisY.LineWidth = 3;
+            chartArea2.AxisY.MajorGrid.LineColor = System.Drawing.SystemColors.ControlDarkDark;
+            chartArea2.AxisY.Title = "Обменнный курс (Руб.)";
+            chartArea2.AxisY.TitleFont = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            chartArea2.AxisY.TitleForeColor = System.Drawing.SystemColors.AppWorkspace;
+            chartArea2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(23)))), ((int)(((byte)(24)))));
+            chartArea2.BorderColor = System.Drawing.SystemColors.AppWorkspace;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend3.Alignment = System.Drawing.StringAlignment.Center;
+            legend3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(23)))), ((int)(((byte)(24)))));
+            legend3.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend3.Font = new System.Drawing.Font("Century Gothic", 16F);
+            legend3.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            legend3.IsTextAutoFit = false;
+            legend3.Name = "Legend1";
+            legend4.Alignment = System.Drawing.StringAlignment.Center;
+            legend4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(23)))), ((int)(((byte)(24)))));
+            legend4.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend4.Font = new System.Drawing.Font("Century Gothic", 16F);
+            legend4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            legend4.IsTextAutoFit = false;
+            legend4.Name = "Legend2";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series3.BorderWidth = 2;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Доллар";
+            series4.BorderWidth = 2;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend2";
+            series4.Name = "Евро";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(1140, 759);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.forecastRange);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.startButton);
+            this.panel1.Controls.Add(this.label);
+            this.panel1.Location = new System.Drawing.Point(1179, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(397, 304);
+            this.panel1.TabIndex = 1;
+            // 
+            // forecastRange
+            // 
+            this.forecastRange.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.forecastRange.Location = new System.Drawing.Point(213, 101);
+            this.forecastRange.Name = "forecastRange";
+            this.forecastRange.Size = new System.Drawing.Size(116, 66);
+            this.forecastRange.TabIndex = 3;
+            this.forecastRange.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(103, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Дни";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.Location = new System.Drawing.Point(58, 198);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(271, 75);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Начать";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label.Location = new System.Drawing.Point(36, 11);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(327, 58);
+            this.label.TabIndex = 0;
+            this.label.Text = "Курсы валют";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1603, 795);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.chart1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox forecastRange;
+    }
+}
+
---
+++ lab 2 (exchange rates)/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+namespace lab_2__exchange_rates_
+{
+
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        double BoxMuller(double mean, double variance)
+        {
+            double z = Math.Cos(2 * Math.PI * rnd.NextDouble()) * Math.Sqrt(-2 * Math.Log(rnd.NextDouble()));
+            return mean + z * Math.Sqrt(variance);
+        }
+
+        private ChartArea chartArea;
+        private Timer timer = new Timer();
+        private int currentIndex = 0;
+        private double rate_USD = 92.05;
+        private double rate_EURO = 99.49;
+        private int range;
+        private double drift = 0.01; // Средний темп роста (дрейф)
+        private double volatility = 0.05; // Волатильность
+        List<double> stat_EURO = new List<double>();
+        List<double> stat_USD = new List<double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Tick += Timer_Tick;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+
+
+            int.TryParse(forecastRange.Text, out int deltaRange);
+            range += deltaRange;
+            
+            double dt = 0.1;
+
+            for (int i = 0; i < deltaRange; i++)
+            {
+                stat_USD.Add(rate_USD);
+
+                double Z_USD = BoxMuller(0, 1);
+                rate_USD *= Math.Exp((drift - 0.5 * volatility * volatility) * dt + volatility * Math.Sqrt(dt) * Z_USD);
+
+                stat_EURO.Add(rate_EURO);
+
+                double Z_EURO = BoxMuller(0, 1);
+                rate_EURO *= Math.Exp((drift - 0.5 * volatility * volatility) * dt + volatility * Math.Sqrt(dt) * Z_EURO);
+            }
+
+            // Настройка графика
+            chartArea = chart1.ChartAreas[0];
+
+            // Установка минимального значения по оси X
+            chartArea.AxisX.Minimum = 0;
+            // Установка максимального значения по оси X
+            chartArea.AxisX.Maximum = range;
+            // Установка размера шага по оси X
+            chartArea.AxisX.Interval = (range < 50) ? 1.0 : range / 50.0;
+
+            // Установка минимального значения по оси Y
+            chartArea.AxisY.Minimum = Math.Min(stat_EURO.Min(), stat_USD.Min());
+            // Установка максимального значения по оси Y
+            chartArea.AxisY.Maximum = Math.Max(stat_EURO.Max(), stat_USD.Max());
+            // Установка размера шага по оси Y
+            chartArea.AxisY.Interval = chartArea.AxisY.Maximum / 100;
+
+            // Инициализация таймера
+            timer.Interval = 5000 / deltaRange;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Обработчик события таймера
+
+            // Проверка на выход за пределы списка
+            if (currentIndex < range)
+            {
+                // Ваш код обработки текущей точки, например, вывод в консоль
+                chart1.Series[0].Points.AddXY(currentIndex, stat_USD[currentIndex]);
+                chart1.Series[1].Points.AddXY(currentIndex, stat_EURO[currentIndex]);
+
+                // Увеличение индекса для следующей итерации
+                currentIndex++;
+            }
+            else
+            {
+                // Если все точки обработаны, остановите таймер
+                timer.Stop();
+            }
+        }
+    }
+}
